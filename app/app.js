fbbc0f763bb68f01223cfc1e849905aed0e41bd5
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-08-08 08:32:47 -0500

    can log in and register can save user info to firebase

--- app/app.js
+++ app/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let app = angular.module("App", ["ngRoute"])
+.constant("FirebaseUrl", "https://todo-task-app.firebaseio.com");
+
+let isAuth = (UserFactory) => {
+  return new Promise ( (resolve, reject) => {
+    UserFactory.isAuthenticated()
+    .then( (userBoolean) => {
+      if (userBoolean) {
+      resolve();
+        } else {
+          reject();
+        }
+    });
+  });
+};
+
+app.config( ($routeProvider) => {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/login.html',
+    controller: 'UserController'
+  })
+  .when('/login/view', {
+    templateUrl: 'partials/profile.html',
+    controller: 'ProfileController'
+  })
+  .otherwise('/');
+});
--- app/factories/UserFactory.js
+++ app/factories/UserFactory.js
@@ -1,1 +1,84 @@
 'use strict';
+
+app.factory("UserFactory", function($q, $http, FirebaseUrl, FBCreds) {
+
+  var config = {
+    apiKey: FBCreds.key,
+    authDomain: FBCreds.authDomain
+  };
+
+  firebase.initializeApp(config);
+
+  let currentUser = null;
+
+  let isAuthenticated = function() {
+    console.log("isAuthenticated called");
+    return new Promise( (resolve, reject) => {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          currentUser = user.uid;
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  };
+
+  let getUser = () => {
+    return currentUser;
+  };
+
+  let createUser = (userObj) => {
+    return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
+    .then((userObj)=>{
+      $http.post(`${FirebaseUrl}/users.json`, JSON.stringify(userObj));
+    })
+    .catch( (err) => {
+      console.log("error", err.message);
+    });
+  };
+
+  let loginUser = (userObj) => {
+    console.log("user", userObj);
+    return $q( (resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
+      .then( (user) => {
+        currentUser = user.uid;
+        resolve(user);
+      })
+      .catch( (err) => {
+        console.log("error", err.message);
+      });
+    });
+  };
+
+  let logoutUser = () => {
+    return firebase.auth().signOut()
+    .catch( (err) => {
+      console.log("error", err.message);
+    });
+  };
+
+  let updateProfileStatus = (profile) => {
+    return $q( (resolve, reject) => {
+      let profileId = profile.id;
+      // PUT the entire obj to FB
+      if (profileId) {
+        $http.put(`${FirebaseUrl}/users/${profileId}.json`,
+          angular.toJson(profile))
+        .then( (data) => {
+          resolve(data);
+        })
+        .catch( (err) => {
+          reject(err);
+        });
+      } else {
+        console.log("I'm burned out for the day. Go home");
+      }
+    });
+  };
+
+  return {isAuthenticated, getUser, createUser, loginUser, logoutUser, updateProfileStatus};
+});
+
