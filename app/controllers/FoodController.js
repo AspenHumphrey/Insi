81948551dc39dc7f1e04056d09dea62fde22cb4c
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-08-11 15:07:24 -0500

    stuff is populating the food pagegit statusgit status yay

--- app/controllers/FoodController.js
+++ app/controllers/FoodController.js
@@ -3,17 +3,55 @@
 app.controller('FoodController', function($scope, $q, $window, $routeParams, FoodFactory){
 
 $scope.search = "";
+$scope.item = 0;
 // $scope.userProfile = data;
 
 
-$scope.calculate = () => {
-	FoodFactory.getFoodData($scope.search)
-	.then( (data) => {
+	$scope.searchFood = () => {
+		FoodFactory.getFoodData($scope.search)
+		.then( (data) => {
+			getFoodArr();
 		// console.log("data", data);
-	});
-};
+		});
+	};
+
+	function getFoodArr() {
+		FoodFactory.getFoodData($scope.search)
+		.then( (foods) => {
+			console.log("foods", foods.data);
+			$scope.foodArr = [];
+			$scope.food = foods.data.hits;
+			console.log("scope.food", $scope.food);
+			angular.forEach($scope.food, function(data){
+				$scope.foodArr.push(data);
+			});
+			console.log("$scope.foodArr", $scope.foodArr);
+		})
+		.catch( (err) => {
+			console.log("err", err);
+		});
+	}
+
+
 
 });
+
+// $scope.breakfast = () => {
+// 	$scope.userProfile.breakfast
+// };
+
+// $scope.lunch = () => {
+// 	// $scope.userProfile.lunch
+// };
+
+// $scope.snack = () => {
+// 		// $scope.userProfile.snack
+
+// };
+
+// $scope.dinner = () => {
+// 	// $scope.userProfile.dinner
+
 
 
 
@@ -23,9 +61,6 @@ $scope.calculate = () => {
 
 // };
 
-// $scope.selectMeal = () => {
-// // {{data.breakfast}} data.lunch data.snack data.dinner
-// };
 
 
 // });
@@ -39,6 +74,6 @@ $scope.calculate = () => {
 // select meal from user profile
 // breakfast lunch snack dinner
 
-// calculate function
+
 
 // select multiple foods to calculate
--- app/factories/FoodFactory.js
+++ app/factories/FoodFactory.js
@@ -15,11 +15,11 @@ app.factory("FoodFactory", function($q, $http, FirebaseUrl, FBCreds){
 
   let getFoodData = (search) => {
       return $q( (resolve, reject) => {
-        $http.get(`https://api.nutritionix.com/v1_1/search/${search}?results=0:20&fields=item_name,brand_name,item_id,nf_total_carbohydrate,nf_serving_size_unit,nf_serving_weight_grams&appId=ee37e8c5&appKey=b7d26d4f522f2374e29448491407fd3e`)
+        $http.get(`https://api.nutritionix.com/v1_1/search/${search}?results=0:20&fields=item_name,brand_name,item_id,nf_total_carbohydrate,nf_serving_size_unit&appId=ee37e8c5&appKey=b7d26d4f522f2374e29448491407fd3e`)
         .then( (data) => {
         	console.log("data", data);
-        	console.log("data.data", data.data);
-          resolve(data.data);
+        	// console.log("data.data", data.data);
+          resolve(data);
         })
         .catch( (err) => {
           console.log("oops", err);
